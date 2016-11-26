067509e40a881e610e6d40ffe79ccf2d493ca1b4
Tae H. Kim <thk301@nyu.edu>
2016-11-25 21:20:01 -0800

    gwgwe

--- nyc/settings.py
+++ nyc/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -83,6 +84,9 @@ DATABASES = {
      }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
                 os.path.join(BASE_DIR, "static"),
@@ -114,10 +118,3 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-        
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-                os.path.join(BASE_DIR, "static"),
-            ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
