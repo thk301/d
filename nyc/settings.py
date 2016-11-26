bff076980ff69e2e9c66ab6ca7fcc8da883e7066
Tae H. Kim <thk301@nyu.edu>
2016-11-25 19:29:12 -0800

    dd

--- nyc/settings.py
+++ nyc/settings.py
@@ -124,12 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-                os.path.join(BASE_DIR, "static"),
-            ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
